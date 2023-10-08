4692e029eb7b7f8de6405e5e6818e34fa49dab40
hunghq <hunghq@yopaz.vn>
2023-10-08 21:53:40 +0700

    Online friends

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import RightSideBar from "../../components/RightSideBar/index.js";
+import { HStack, Box } from "@chakra-ui/react";
 
 export default function Login() {
   return (
-    <div>
+    <HStack align="right" w="ful">
+      <Box flex="1"></Box>
       <RightSideBar></RightSideBar>
-    </div>
+    </HStack>
   );
 }
