0e4a0d28a42d201cf63840b0bc48dc3a30c1fef0
Hoang Sy Quy <kiaisoft.quy.hoang@gmail.com>
2023-10-14 16:37:45 +0700

    fix/ui: fix css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Register from "./pages/Register";
-import Login from "./pages/Login"
-import ForgotPassword from "./pages/ForgotPassword";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
